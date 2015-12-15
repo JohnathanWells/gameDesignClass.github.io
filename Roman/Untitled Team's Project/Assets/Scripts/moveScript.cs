af232f9be707419e9c5d23f8efb013c0a4e81f8e
Roman <Romanreiner9@gmail.com>
2015-12-14 20:06:14 -0500

    Revert "Revert "this is my latest build""
    This reverts commit 42ff92ebf2fe7e3eebf5eca9b1d60c95e294fddf.

---
+++ Roman/Untitled Team's Project/Assets/Scripts/moveScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveScript : MonoBehaviour {
+
+    public Rigidbody2D bulletLeft;
+    public Rigidbody2D bulletRight;
+
+    public float speedLeft = -1;
+    public float speedRight = 1;
+
+
+	// Use this for initialization
+	void Start () {
+
+        bulletLeft.velocity = new Vector3(speedLeft, 0, 0);
+        bulletRight.velocity = new Vector3(speedRight, 0, 0);
+
+        /*if(GameObject.Find("Logic").GetComponent<Logic>().collision(new Rect(transform.position.x/8-3/8, transform.position.y/8-3/8, 3/8, 3/8), .1f) )
+        {
+            Destroy(bulletLeft);
+            Destroy(bulletRight);
+        }*/
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (GameObject.Find("Logic").GetComponent<Logic>().destructableCollision(new Rect(transform.position.x / 4, transform.position.y / 4, 0, 0), .1f))
+        {
+            Debug.Log("q");
+            GameObject.Find("Logic").GetComponent<Logic>().setTile((int)Mathf.Round(transform.position.x / 4), (int)Mathf.Round(transform.position.y / 4), "");
+            Destroy(gameObject);
+        }
+        if (GameObject.Find("Logic").GetComponent<Logic>().collision(new Rect(transform.position.x / 4 - 3 / 8, transform.position.y / 4 - 3 / 8, 3 / 8, 3 / 8), .1f))
+        {
+            //Destroy(bulletLeft);
+            //Destroy(bulletRight);
+            Destroy(gameObject);
+        }
+	
+	}
+}
---
+++ Roman/Untitled Team's Project/Assets/statusIndacator.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class statusIndacator : MonoBehaviour {
+
+    public RectTransform healthBarRect;
+    public Text healthText;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+        if (healthBarRect == null)
+        {
+            Debug.LogError("STAUTS INDAICATOR: No health bar object referenced!");
+        }
+	
+	}
+
+    public void SetHealth(int _cur, int _max)
+    {
+        float _value = (float)_cur / _max;
+
+        healthBarRect.localScale = new Vector3(_value, healthBarRect.localScale.y, healthBarRect.localScale.z);
+        healthText.text = _cur + "/" + _max + "HP";
+    }
+	
+}
