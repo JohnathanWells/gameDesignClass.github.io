8ef936889c6717ac94a213b483dc39e00d2a5fa5
Roman <Romanreiner9@gmail.com>
2015-12-14 21:07:23 -0500

    Sooo wtf work

---
+++ Untitled Team's Project/Assets/menuScripts/mainScreenCam.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class mainScreenCam : MonoBehaviour {
+
+    public float orthographicSize = 5;
+    public float aspect = 1.33333f;
+    public Camera mainCam;
+
+	// Use this for initialization
+	void Start () {
+        Camera.main.projectionMatrix = Matrix4x4.Ortho(
+                   -orthographicSize * aspect, orthographicSize * aspect,
+                   -orthographicSize, orthographicSize,
+                   mainCam.nearClipPlane, mainCam.farClipPlane);
+ }
+	
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
